6a91de370f9f8ed259f3d9ba3239e88733d92fda
iwilliams83 <indirajamurath@gmail.com>
2018-06-07 00:54:07 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -98,10 +98,10 @@ class Person
     #accept two args
     #if topic is politics, both ppl get sadder, return a string
     if topic == "politics"
-      # self.happiness=(self.happiness - 2)
-      # person.happiness=(person.happiness - 2)
-      self.happiness -= 2
-      person.happiness -= 2
+      self.happiness = self.happiness - 2
+      person.happiness = person.happiness - 2
+      # self.happiness -= 2
+      # person.happiness -= 2
       puts "@happiness #{@happiness}"
       puts "person.happiness #{person.happiness}"
       self.happiness = (@happiness)
